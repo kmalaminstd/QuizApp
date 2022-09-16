5049ae1973bde7dc5289fbc21f57527b51cc4591
kmalamin <alaminkhanstd@gmail.com>
2022-09-17 00:28:20 +0600

    first full version quiz app

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,17 @@ import quizArr from "./allQuiz.js"
 
 // front page function
 
+let quizQusNo = 0
+let quizScore = 0
+
 // selectors
 const rulesElm = document.querySelector('.rules')
 const rulsBtnElm = document.querySelector('.rulsButton button')
 const gameStartSecElm = document.querySelector('.frontGameBtn')
 const gameStartBtnElm = document.querySelector('.playStart')
 const quizSecElm = document.querySelector('.quizSection')
-const loadingbarElm = document.querySelector('.loadingBar')
 const quizTimeElm = document.querySelector('.timeSec')
 
-let quizQusNo = 0
 
 //function
 const gameFrontPageRulsFunc = () => {
@@ -24,18 +25,17 @@ const gameFrontPageRulsFunc = () => {
         gameStartSecElm.style.display = 'none'
         quizSecElm.style.display = 'block'
         quizTimeElm.style.display = 'block'
-        loadingBarInc()
+        quizTimeElm.style.display = 'block'
+        
         quizTimeFunc()
-        gettingQuizQues()
+        quizQuesShowInUi(quizArr[quizQusNo])
+        
     })
 }
 
 gameFrontPageRulsFunc()
 
-// loading bar function
-const loadingBarInc = () => {
-    loadingbarElm.classList.toggle('incLoadingBar')
-}
+
 
 // stopwatch time generate
 
@@ -48,7 +48,7 @@ const quizSecShow = document.querySelector('.sec')
 
 const quizTimeFunc = () => {
     setInterval(startTime, 10)
-    
+
 }
 
 const startTime = () => {
@@ -68,91 +68,96 @@ const startTime = () => {
         quizSecShow.innerHTML = quizSec
         quizSec = 0
         quizMinsShow.innerHTML = quizMin
-        // console.log(quizMin);kk
+        
         if(quizMin == 59){
             quizQusNo++
             quizMin = 0
-            gettingQuizQues(quizQusNo)
+            console.log(quizQusNo);
             quizQuesShowInUi(quizArr[quizQusNo])
         }
     }
+
     
-    // console.log(quizSec);
 }
 
 // radio button validation
 
 // selectors
-const singleOption = document.querySelectorAll('.singleOption input')
-
-// document.querySelectorAll('.singleOption input')[1].checked = true
 
 
-
-
-function radioValid (radioBtn) {
-    console.log(radioBtn);
-    for(let i = 0; i < singleOption.length; i++){
+function radioValid () {
+    const radioBtn = document.querySelectorAll('.quizSection form .singleOption input')
     
-        singleOption[i].addEventListener('click', e => {
-            if(singleOption[i].className === 'op1'){
+    for(let i = 0; i < radioBtn.length; i++){
+
+        radioBtn[i].addEventListener('click', e => {
+            if(radioBtn[i].className === 'op1'){
                document.getElementById('op1').checked = true
             }else{
                 document.getElementById('op1').checked = false
             }
-    
-            if(singleOption[i].className === 'op2'){
+
+            if(radioBtn[i].className === 'op2'){
                 document.getElementById('op2').checked = true
              }else{
                  document.getElementById('op2').checked = false
              }
-    
-             if(singleOption[i].className === 'op3'){
+
+             if(radioBtn[i].className === 'op3'){
                 document.getElementById('op3').checked = true
              }else{
                  document.getElementById('op3').checked = false
              }
-    
-             if(singleOption[i].className === 'op4'){
+
+             if(radioBtn[i].className === 'op4'){
                 document.getElementById('op4').checked = true
              }else{
                  document.getElementById('op4').checked = false
              }
         })
-        
+
     }
 }
 
 
-const gettingQuizQues = () => {
-    // for(let i = 0; i < quizArr.length; i++){
-    //     console.log(quizArr[1]);
-    // }
-    quizQuesShowInUi(quizArr[quizQusNo])
-    
-    console.log(quizArr[quizQusNo])
-    
+
+// selectors
+
+
+function quesNextSkip() {
+    const skipBtn = document.querySelector('.skipBtn')
+
+    skipBtn.addEventListener('click', (e) => {
+        document.querySelector('.rightAns').innerHTML = ''
+        quizQusNo++;
+        
+        if(quizQusNo === quizArr.length){
+            quizQusNo = 0
+            document.querySelector('.quizSection').style.display = 'none'
+            document.querySelector('.quizSecButtons').style.display = 'none'
+            quizScoreFunc()
+            document.querySelector('.quizScoreSec button').addEventListener('click', () => {
+                window.location.reload()
+            })
+            quizTimeElm.style.display = 'none'
+            
+        }
+        
+        quizQuesShowInUi(quizArr[quizQusNo])
+        quizMin = 0,
+        quizSec = 0
+        
+    }) 
 }
 
-// selectors 
-const quizSecBtnELm = document.querySelector('.quizSecButtons')
+quesNextSkip()
 
-// const quesNextBtn = document.querySelector('.nextBtn')
-// const quesSkipBtn = document.querySelector('.skipBtn')
 
-function quesNextSkip (btns) {
-    // quesNextBtn.addEventListener('click', ()=> {
-    //     quizQusNo++
-    // })
-    // quesSkipBtn.addEventListener('click', () => {
-    //     quizQusNo++
-    // })
-
-    console.log(btns);
+// quizScore 
+function quizScoreFunc(){
+    document.querySelector('.quizScoreSec').style.display = 'block'
+    document.querySelector('.quizScoreSec #score').textContent = quizScore
 }
-
-    
-
 
 /// question show in ui
 
@@ -160,17 +165,17 @@ function quesNextSkip (btns) {
 
 
 function quizQuesShowInUi (ques)  {
-    const quizPlaceElm = document.querySelector('.quizSection')
-    // document.querySelector('.quizSection').innerHTML = ''
-    console.log(ques);
+
+    let quizPlaceElm = document.querySelector('.quizSection form')
+    quizPlaceElm.innerHTML = ''
     let htmlElm = `
-        <div class="singleQuiz">
+            <div class="singleQuiz">
             <div class="quizQus">
                 <P><Span>${ques.id}</Span>. ${ques.QuizQues}</P>
             </div>
             <div class="quizOption">
                 <div class="singleOption">
-                    <input type="radio" value=${ques.Option1} class="op1" id="op1">
+                    <input type="radio" value="${ques.Option1}" class="op1" id="op1">
                     <label for="fOp">${ques.Option1}</label>
                 </div>
 
@@ -189,26 +194,73 @@ function quizQuesShowInUi (ques)  {
                     <label for="fOp">${ques.Option4}</label>
                 </div>
             </div>
+            </div>
+        <div class="formBtn">
+            <button class="id-${ques.id}">Submit</button>
         </div>
 
-            <div class="quizSecButtons">
-                <button class="skipBtn">Skip</button>
-                <button class="nextBtn">Next</button>
-            </div>
-       
     `
 
-    
-
+    // quizPlaceElm.innerHTML = htmlElm
     quizPlaceElm.insertAdjacentHTML('beforeend', htmlElm)
-    
-    // console.log(document.querySelector('.quizSecButtons'));
-    console.log(document.querySelectorAll('.singleOption input'));
-    radioValid(document.querySelectorAll('.singleOption input'))
-    quesNextSkip(document.querySelector('.quizSecButtons'))
+    radioValid()
     
 }
 
+// form button disable 
+function buttonDisable(){
+    document.querySelector('form button').addEventListener('click', ()=> {
+        document.querySelector('form button').setAttribute('disabled', '')
+    })
+}
 
+function gettingAnswerFromQuiz (){
+    const quizOp = document.querySelectorAll('.singleOption')
+
+    console.log(quizOp[1].querySelector('input'));
+}
+
+let formAns ;
+
+function formValue(){
+    if(document.querySelector('.quizSection form')){
+        document.querySelector('.quizSection form').addEventListener('submit', e => {
+            e.preventDefault()
+            const id = getId(document.querySelector('form button'))
+
+            buttonDisable()
+            const quizQus = document.querySelectorAll('.quizSection form .singleOption input')
+            for(let i = 0; i < quizQus.length; i++){
+                if(quizQus[i].checked){
+                    formAns = quizQus[i].value
+                    matchAns(id)
+                    // getValueFromOp()
+                    
+                }
+            }
+        }) 
+
+    }
+}
+
+formValue()
+
+
+
+// get answer from id
+function matchAns(id) {
+    if(formAns === quizArr[id].RightAnswer){
+        quizScore++
+    }else{
+        document.querySelector('.rightAns').innerHTML = `<p>Right Answer is : <br> <b>${quizArr[id].RightAnswer}</b></p>`
+    }
+    
+}
+
+// get id
+function getId(idTarget){
+    // console.log(idTarget.classList[0].split('-')[1]);
+    return idTarget.classList[0].split('-')[1]-1
+}
 
 
